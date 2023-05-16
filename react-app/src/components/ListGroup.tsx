bcd835230b92465432359d1ef2c77e21dc88f4c3
Julien Bogiraud <julien@bogiraud.com>
2023-05-16 15:50:13 +0200

    conditionnal display

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,15 +1,24 @@
 function ListGroup() {
-  const items = ["NY", "SF", "London", "Paris", "Lyon"];
+  let items = ["NY", "SF", "London", "Paris", "Lyon"];
+  //  items = [];
+
+  function getItems() {
+    return items.map((item) => (
+      <li key={item} className="list-group-item">
+        {item}
+      </li>
+    ));
+  }
+
+  function noItems() {
+    return <p>No Items Found</p>;
+  }
 
   return (
     <>
       <h1> List </h1>
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
-            {item}
-          </li>
-        ))}
+        {items.length !== 0 ? getItems() : <p>No Items Found</p>}
       </ul>
     </>
   );
