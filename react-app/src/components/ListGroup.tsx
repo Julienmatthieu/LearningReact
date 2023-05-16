5389ad6534ff3dbc9c2620a04e7b0d27ab80e6c6
Julien Bogiraud <julien@bogiraud.com>
2023-05-16 16:10:28 +0200

    State and Oncclick

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,14 +1,24 @@
+import { useState, useSyncExternalStore } from "react";
+
 function ListGroup() {
-  let items = ["NY", "SF", "London", "Paris", "Lyon"];
-  //items = [];
+  const items = ["New York", "London", "Paris", "Lyon", "Tokyo"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1> List </h1>
       <ul className="list-group">
         {items.length === 0 && <h1>No Item Found</h1>}
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li
+            key={index}
+            className={
+              "list-group-item " + (selectedIndex === index && "active")
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
