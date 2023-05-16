1677246b94ffb843c89a084ed16e07e1119739b1
Julien Bogiraud <julien@bogiraud.com>
2023-05-16 15:54:45 +0200

    updating on dynamique display

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,24 +1,17 @@
 function ListGroup() {
   let items = ["NY", "SF", "London", "Paris", "Lyon"];
-  //  items = [];
-
-  function getItems() {
-    return items.map((item) => (
-      <li key={item} className="list-group-item">
-        {item}
-      </li>
-    ));
-  }
-
-  function noItems() {
-    return <p>No Items Found</p>;
-  }
+  //items = [];
 
   return (
     <>
       <h1> List </h1>
       <ul className="list-group">
-        {items.length !== 0 ? getItems() : <p>No Items Found</p>}
+        {items.length === 0 && <h1>No Item Found</h1>}
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
