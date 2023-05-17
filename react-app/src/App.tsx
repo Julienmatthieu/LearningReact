c31e312bec9fcce66f0fcc7bbc3af254df1afdfa
Julien Bogiraud <julien@bogiraud.com>
2023-05-17 18:44:33 +0200

    Form with no reload

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,20 +1,10 @@
 import ExpendableText from "./components/ExpendableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpendableText maxChars={10}>
-        Bacon ipsum dolor amet pig ribeye turkey, shank ground round chuck
-        meatball pastrami ball tip rump boudin kevin leberkas tri-tip bacon.
-        Tail burgdoggen ham filet mignon turkey sausage shankle shank capicola
-        prosciutto rump brisket pancetta. Corned beef ribeye picanha meatloaf.
-        Doner porchetta salami short loin, ham hock frankfurter pork meatball
-        picanha chuck cupim tail shoulder venison turducken. Sirloin picanha
-        pork belly, hamburger turducken salami pork loin swine pancetta corned
-        beef fatback. Ham leberkas spare ribs meatball cupim t-bone ham hock,
-        buffalo pork chop tenderloin meatloaf brisket tri-tip andouille pork
-        belly.
-      </ExpendableText>
+      <Form></Form>
     </div>
   );
 }
