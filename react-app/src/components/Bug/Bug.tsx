5cf802fd0c42ddfc3b7c674c8faaf520ce9006eb
Julien Bogiraud <julien@bogiraud.com>
2023-05-17 16:24:06 +0200

    fixin edit list

--- react-app/src/components/Bug/Bug.tsx
+++ react-app/src/components/Bug/Bug.tsx
@@ -2,19 +2,28 @@ import { useState } from "react";
 
 export const Bug = () => {
   const [bugs, setBugs] = useState([
-    { id: 1, title: " bug 1", fixed: false },
-    { id: 2, title: " bug 2", fixed: false },
+    { id: 0, title: " bug 1", fixed: false },
+    { id: 1, title: " bug 2", fixed: false },
+    { id: 2, title: " bug 3", fixed: false },
+    { id: 3, title: " bug 4", fixed: false },
   ]);
 
   const handleClick = (id: number) => {
-    setBugs(bugs.map((bug) => (bug.id == id ? tmp : bug)));
+    console.log("here");
+    setBugs(
+      bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: !bug.fixed } : bug))
+    );
   };
 
   return (
     <div>
       Bug
       <button onClick={() => handleClick(1)}>Fixing</button>
-      {String(bugs[1].fixed)}
+      {bugs.map((bug) => (
+        <p>
+          Bug id: {bug.id} and Fixed: {String(bug.fixed)}
+        </p>
+      ))}
     </div>
   );
 };
