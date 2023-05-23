61972e14ac54e9cf84dfa5cc47b3a7df1a5f792d
Julien Bogiraud <julien@bogiraud.com>
2023-05-23 16:58:15 +0200

    using await try/catch on axios

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 interface User {
   id: number;
@@ -10,14 +10,24 @@ function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
 
-  useEffect(() => {
-    // Fake data website
-    // 'then will be executed when the promess is resolve
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users*")
-      .then((res) => setUsers(res.data))
-      .catch((err) => setError(err.message));
-  }, []);
+  useEffect(
+    /*we can not pass an async funct here*/ () => {
+      // get -> promise -> res / error
+      // get -> await promise -> res / error
+      const fetchUsers = async () => {
+        try {
+          const res = await axios.get<User[]>(
+            "https://jsonplaceholder.typicode.com/zusers"
+          );
+          setUsers(res.data);
+        } catch (err) {
+          setError((err as AxiosError).message);
+        }
+      };
+      fetchUsers();
+    },
+    []
+  );
 
   return (
     <>
