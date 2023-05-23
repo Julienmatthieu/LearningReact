87d40a2674e3f92909d12db9d296f4b8a588d738
Julien Bogiraud <julien@bogiraud.com>
2023-05-23 12:30:04 +0200

    ExpensesList

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,20 @@
-import ExpendableText from "./components/ExpendableText";
-import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import { useState } from "react";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 1, category: "A" },
+    { id: 2, description: "abb", amount: 4, category: "B" },
+    { id: 3, description: "bbb", amount: 5, category: "C" },
+    { id: 4, description: "bbc", amount: 10, category: "A" },
+  ]);
+
   return (
     <div>
-      <Form></Form>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id != id))}
+      ></ExpenseList>
     </div>
   );
 }
